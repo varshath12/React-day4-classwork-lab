38aa70c18b199077773ae69ed1e9eccc3056f462
varshath12 <108916881+varshath12@users.noreply.github.com>
2023-03-29 12:08:11 +0530

    Add files via upload

---
+++ App (5).js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import D4CE from '/.D4CE'
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <center>
+          <D4CE/>
+        </center>
+      </div>
+    )
+  }
+}
+export default App
